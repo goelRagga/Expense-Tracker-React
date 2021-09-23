fb04393c5709c9b1261879a354c2bf994b3e3704
goelRagga <raghavgoel847@gmail.com>
2021-09-23 17:21:38 +0530

    Layout Done

---
+++ src/components/Main/Form/Form.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { TextField, Typography, Grid, Button, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
+
+import useStyles from './styles'
+
+const Form = () => {
+    const classes = useStyles();
+
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={12}>
+                <Typography>
+                    .....
+                </Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <FormControl fullWidth>
+                    <InputLabel>Type</InputLabel>
+                    <Select>
+                        <MenuItem value="Income">Income</MenuItem>
+                        <MenuItem value="Expense">Expense</MenuItem>
+                    </Select>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+                <FormControl fullWidth>
+                    <InputLabel>Cataegory</InputLabel>
+                    <Select>
+                        <MenuItem value="business"> Business </MenuItem>
+                        <MenuItem value="salary"> Salary </MenuItem>
+                    </Select>
+                </FormControl>
+            </Grid>
+            <Grid item xs={6}>
+                <TextField type="number" label="Amount" fullwidth/>
+            </Grid>
+            <Grid item xs={6}>
+                <TextField type="date" label="Date" fullwidth/>
+            </Grid>
+            <Button className={classes.button} variant="outlined" color="primary" fullWidth>Create</Button>
+        </Grid>
+    )
+}
+
+export default Form
